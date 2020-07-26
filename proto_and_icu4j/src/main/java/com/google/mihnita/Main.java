734685cc9020d9d0031b590c09e5161730807895
Mihai Nita <mihnita@gmail.com>
2020-07-26 16:32:31 -0700

    Add support for multiple selectors

--- proto_and_icu4j/src/main/java/com/google/mihnita/Main.java
+++ proto_and_icu4j/src/main/java/com/google/mihnita/Main.java
@@ -1,64 +1,74 @@
 package com.google.mihnita;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.mihnita.msgfmt_proto.MessageFormat.Cases;
 import com.google.mihnita.msgfmt_proto.MessageFormat.MFDM;
 import com.google.mihnita.msgfmt_proto.MessageFormat.Part;
 import com.google.mihnita.msgfmt_proto.MessageFormat.Placeholder;
 import com.google.mihnita.msgfmt_proto.MessageFormat.PlainText;
+import com.google.mihnita.msgfmt_proto.MessageFormat.SelectorMessage;
+import com.google.mihnita.msgfmt_proto.MessageFormat.SimpleMessage;
+import com.google.mihnita.msgfmt_proto.MessageFormat.Switch;
+import com.google.mihnita.msgfmt_proto.MessageFormat.Cases.Case;
 import com.google.mihnita.msgfmt_proto.MessageFormat2;
 import com.ibm.icu.text.MessageFormat;
 
 public class Main {
 
-	public static Part.Builder text(String text) {
-		return Part.newBuilder().setPlainText(PlainText.newBuilder().setValue(text));
+	public static Part text(String text) {
+		return Part.newBuilder().setPlainText(PlainText.newBuilder().setValue(text)).build();
 	}
 
-	public static Part.Builder ph(String name) {
+	public static Part ph(String name) {
 		return ph(name, null, null);
 	}
 
-	public static Part.Builder ph(String name, String type) {
+	public static Part ph(String name, String type) {
 		return ph(name, type, null);
 	}
 
-	public static Part.Builder ph(String name, String type, Map<String, String> knobs) {
+	public static Part ph(String name, String type, Map<String, String> knobs) {
 		Placeholder.Builder ph = Placeholder.newBuilder();
 		ph.setName(name);
 		if (type != null) ph.setType(type);
 		if (knobs != null) ph.putAllKnobs(knobs);
-		return Part.newBuilder().setPlaceholder(ph);
+		return Part.newBuilder().setPlaceholder(ph).build();
 	}
 
-	public static void main(String[] args) {
-		
+	public static boolean messageWithPlaceholdersNoSelect() {
 
-		String icuPattern = "Hello {user}, in {locale} today is ''{today,time,::yMMMdjmszzzz}''.\n"
+		// This is the ICU pattern
+		String icuMessage = "Hello {user}, in {locale} today is ''{today,time,::yMMMdjmszzzz}''.\n"
 				+ "A big number would be {bigCount, number, currency}.\n"
 				+ "An ordinal is {count, spellout}.\n";
 
-		MFDM.Builder protoMessage = MFDM.newBuilder();
-		protoMessage.addParts(text("Hello "));
-		protoMessage.addParts(ph("user"));
-		protoMessage.addParts(text(", in "));
-		protoMessage.addParts(ph("locale"));
-		protoMessage.addParts(text(" today is '"));
-		protoMessage.addParts(ph("today", "time", ImmutableMap.of("skeleton", "yMMMdjmszzzz")));
-		protoMessage.addParts(text("'.\nA big number would be "));
-		protoMessage.addParts(ph("bigCount", "number", ImmutableMap.of("type", "currency")));
-		protoMessage.addParts(text(".\nAn ordinal is "));
-		protoMessage.addParts(ph("count", "spellout"));
-		protoMessage.addParts(text(".\n"));
-		
-//		System.out.println(mfdm);
+		// Here we build the proto message
+		SimpleMessage simpleMessage = SimpleMessage.newBuilder()
+			.addParts(text("Hello "))
+			.addParts(ph("user"))
+			.addParts(text(", in "))
+			.addParts(ph("locale"))
+			.addParts(text(" today is '"))
+			.addParts(ph("today", "time", ImmutableMap.of("skeleton", "yMMMdjmszzzz")))
+			.addParts(text("'.\nA big number would be "))
+			.addParts(ph("bigCount", "number", ImmutableMap.of("type", "currency")))
+			.addParts(text(".\nAn ordinal is "))
+			.addParts(ph("count", "spellout"))
+			.addParts(text(".\n"))
+			.build();
 
+		MFDM protoMessage = MFDM.newBuilder().setSimpleMessage(simpleMessage).build();
+
+		// Prepare locale for formatters
 		Locale locale = Locale.forLanguageTag("de-DE");
 
+		// Prepare arguments for formatting
 		Map<String, Object> msgArgs = new HashMap<>();
 		msgArgs.put("user", "Mihai");
 		msgArgs.put("count", 142);
@@ -67,17 +77,137 @@ public class Main {
 		msgArgs.put("bigCount", 1234567890.97531);
 		msgArgs.put("today", new Date());
 
-		System.out.println("ICU pattern:\n" + icuPattern);
-		
-		MessageFormat mf = new MessageFormat(icuPattern, locale);
-		String result = mf.format(msgArgs);
-		System.out.println("ICU result:\n" + result);
+		System.out.println("===== ICU Message:\n" + icuMessage);
+		// Disable, this is too verbose. Keep it for debugging
+		// System.out.println("Proto Message:\n" + protoMessage);
 
-		MessageFormat2 mf2 = new MessageFormat2(protoMessage.build(), locale);
-		String result2 = mf2.format(msgArgs);
-		System.out.println("Proto result:\n" + result2);
-		
-		if (!result.equals(result2))
+		// Format using the ICU MessageFormat
+		MessageFormat mfIcu = new MessageFormat(icuMessage, locale);
+		String resultIcu = mfIcu.format(msgArgs);
+		System.out.println("# ICU result:\n" + resultIcu);
+
+		// Format using the new Proto MessageFormat
+		MessageFormat2 mfProto = new MessageFormat2(protoMessage, locale);
+		String resultProto = mfProto.format(msgArgs);
+		System.out.println("# Proto result:\n" + resultProto);
+
+		return resultIcu.equals(resultProto);
+	}
+
+	public static boolean messageWithSeveralSelectors() {
+
+		// This is the ICU pattern
+		String icuMessage = ""
+				+ "{host_gender, select,\n"
+				+ "  female {{guest_count, plural,\n"
+				+ "    =1    {{host_name} invited {guest_count} person to her party.}\n"
+				+ "    other {{host_name} invited {guest_count} people to her party.}\n"
+				+ "  }}\n"
+				+ "  male   {{guest_count, plural,\n"
+				+ "    =1    {{host_name} invited {guest_count} person to his party.}\n"
+				+ "    other {{host_name} invited {guest_count} people to his party.}\n"
+				+ "  }}\n"
+				+ "  other  {{guest_count, plural,\n"
+				+ "    =1    {{host_name} invited {guest_count} person to their party.}\n"
+				+ "    other {{host_name} invited {guest_count} people to their party.}\n"
+				+ "  }}\n"
+				+ "}";
+
+		// Here we build the proto message
+		Part[] parts_female_singular = {
+				ph("host_name"), text(" invited "), ph("guest_count"), text(" person to her party.")
+		};
+		Part[] parts_female_plural = {
+				ph("host_name"), text(" invited "), ph("guest_count"), text(" people to her party.")
+		};
+		Part[] parts_male_singular = {
+				ph("host_name"), text(" invited "), ph("guest_count"), text(" person to his party.")
+		};
+		Part[] parts_male_plural = {
+				ph("host_name"), text(" invited "), ph("guest_count"), text(" people to his party.")
+		};
+		Part[] parts_other_singular = {
+				ph("host_name"), text(" invited "), ph("guest_count"), text(" person to their party.")
+		};
+		Part[] parts_other_plural = {
+				ph("host_name"), text(" invited "), ph("guest_count"), text(" people to their party.")
+		};
+		Cases.Builder cases_fs = Cases.newBuilder()
+				.addCase(Case.newBuilder().setAlpha("female")).addCase(Case.newBuilder().setNumeric(1));
+		Cases.Builder cases_fp = Cases.newBuilder()
+				.addCase(Case.newBuilder().setAlpha("female")).addCase(Case.newBuilder().setAlpha("other"));
+		Cases.Builder cases_ms = Cases.newBuilder()
+				.addCase(Case.newBuilder().setAlpha("male")).addCase(Case.newBuilder().setNumeric(1));
+		Cases.Builder cases_mp = Cases.newBuilder()
+				.addCase(Case.newBuilder().setAlpha("male")).addCase(Case.newBuilder().setAlpha("other"));
+		Cases.Builder cases_os = Cases.newBuilder()
+				.addCase(Case.newBuilder().setAlpha("other")).addCase(Case.newBuilder().setNumeric(1));
+		Cases.Builder cases_op = Cases.newBuilder()
+				.addCase(Case.newBuilder().setAlpha("other")).addCase(Case.newBuilder().setAlpha("other"));
+		SelectorMessage sm = SelectorMessage.newBuilder()
+				.addSwitch(Switch.newBuilder().setName("host_gender").setType("select"))
+				.addSwitch(Switch.newBuilder().setName("guest_count").setType("plural"))
+				.addCases(cases_fs)
+				.addMessages(SimpleMessage.newBuilder().addAllParts(Arrays.asList(parts_female_singular)))
+				.addCases(cases_fp)
+				.addMessages(SimpleMessage.newBuilder().addAllParts(Arrays.asList(parts_female_plural)))
+				.addCases(cases_ms)
+				.addMessages(SimpleMessage.newBuilder().addAllParts(Arrays.asList(parts_male_singular)))
+				.addCases(cases_mp)
+				.addMessages(SimpleMessage.newBuilder().addAllParts(Arrays.asList(parts_male_plural)))
+				.addCases(cases_os)
+				.addMessages(SimpleMessage.newBuilder().addAllParts(Arrays.asList(parts_other_singular)))
+				.addCases(cases_op)
+				.addMessages(SimpleMessage.newBuilder().addAllParts(Arrays.asList(parts_other_plural)))
+				.build();
+
+		MFDM protoMessage = MFDM.newBuilder().setSelectorMessage(sm).build();
+
+		// Prepare locale for formatters
+		Locale locale = Locale.forLanguageTag("de-DE");
+
+
+		System.out.println("===== ICU Message:\n" + icuMessage + "\n");
+		// Disable, this is too verbose. Keep it for debugging
+		// System.out.println("Proto Message:\n" + protoMessage);
+
+		// Format using the ICU MessageFormat
+		MessageFormat mfIcu = new MessageFormat(icuMessage, locale);
+
+		// Format using the new Proto MessageFormat
+		MessageFormat2 mfProto = new MessageFormat2(protoMessage, locale);
+
+		boolean success = true;
+		success &= formatAndCompare(mfIcu, mfProto, "Maria", "female", 1);
+		success &= formatAndCompare(mfIcu, mfProto, "Maria", "female", 12);
+		success &= formatAndCompare(mfIcu, mfProto, "John",   "male",   1);
+		success &= formatAndCompare(mfIcu, mfProto, "John",   "male",  12);
+		success &= formatAndCompare(mfIcu, mfProto, "XyZvw", "other",   1);
+		success &= formatAndCompare(mfIcu, mfProto, "XyZvw", "undef",  12);
+		return success;
+	}
+
+	static boolean formatAndCompare(MessageFormat mfIcu, MessageFormat2 mfProto,
+			String host_name, String host_gender, double guest_count) {
+		// Prepare arguments for formatting
+		Map<String, Object> msgArgs = new HashMap<>();
+		msgArgs.put("host_name", host_name);
+		msgArgs.put("host_gender", host_gender);
+		msgArgs.put("guest_count", guest_count);
+
+		String resultIcu = mfIcu.format(msgArgs);
+		System.out.println("ICU result   : " + resultIcu);
+		String resultProto = mfProto.format(msgArgs);
+		System.out.println("Proto result : " + resultProto);
+		System.out.println();
+
+		return resultIcu.equals(resultProto);
+	}
+
+	public static void main(String[] args) {
+		if (!messageWithPlaceholdersNoSelect())
+			System.out.println("RESULT DIFFERENCES!");
+		if (!messageWithSeveralSelectors())
 			System.out.println("RESULT DIFFERENCES!");
 	}
 }
--- proto_and_icu4j/src/main/java/com/google/mihnita/msgfmt_proto/MessageFormat2.java
+++ proto_and_icu4j/src/main/java/com/google/mihnita/msgfmt_proto/MessageFormat2.java
@@ -1,19 +1,31 @@
 package com.google.mihnita.msgfmt_proto;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.StringJoiner;
 
 import com.google.mihnita.msgfmt_proto.DateTimeFormatter2.DateTimeFormatterFactory;
+import com.google.mihnita.msgfmt_proto.MessageFormat.Cases;
+import com.google.mihnita.msgfmt_proto.MessageFormat.Cases.Case;
+import com.google.mihnita.msgfmt_proto.MessageFormat.CasesOrBuilder;
 import com.google.mihnita.msgfmt_proto.MessageFormat.MFDM;
 import com.google.mihnita.msgfmt_proto.MessageFormat.Part;
 import com.google.mihnita.msgfmt_proto.MessageFormat.Placeholder;
+import com.google.mihnita.msgfmt_proto.MessageFormat.SelectorMessage;
+import com.google.mihnita.msgfmt_proto.MessageFormat.SimpleMessage;
+import com.google.mihnita.msgfmt_proto.MessageFormat.Switch;
 import com.google.mihnita.msgfmt_proto.NumberFormatter2.NumberFormatterFactory;
+import com.ibm.icu.text.PluralRules;
+import com.ibm.icu.text.PluralRules.PluralType;
 
 public class MessageFormat2 {
 	private final MFDM mfdm;
 	private final Locale locale;
-	
+
 	private static final Map<String, IFormatterFactory> DEFAULT_KNOWN_FORMATTERS = new HashMap<>();
 	static {
 		DateTimeFormatterFactory dff = new DateTimeFormatter2.DateTimeFormatterFactory();
@@ -23,7 +35,7 @@ public class MessageFormat2 {
 		DEFAULT_KNOWN_FORMATTERS.put("number", nff);
 		DEFAULT_KNOWN_FORMATTERS.put("spellout", nff);
 		DEFAULT_KNOWN_FORMATTERS.put("ordinal", nff);
-		DEFAULT_KNOWN_FORMATTERS.put("duration", nff);
+//		DEFAULT_KNOWN_FORMATTERS.put("duration", nff);
 	}
 
 	public MessageFormat2(MFDM mfdm, Locale locale) {
@@ -32,8 +44,18 @@ public class MessageFormat2 {
 	}
 
 	public String format(Map<String, Object> args) {
+		if (mfdm.hasSimpleMessage())
+			return formatSimpleMessage(args);
+		else
+			return formatSelectorMessage(args);
+	}
+
+	public String formatSimpleMessage(Map<String, Object> args) {
+		return formatSimpleMessageImpl(mfdm.getSimpleMessage(), args);
+	}
+	public String formatSimpleMessageImpl(SimpleMessage simpleMessage, Map<String, Object> args) {
 		StringBuilder result = new StringBuilder();
-		for (Part part : mfdm.getPartsList()) {
+		for (Part part : simpleMessage.getPartsList()) {
 			if (part.hasPlainText())
 				result.append(part.getPlainText().getValue());
 			else if (part.hasPlaceholder()) {
@@ -41,6 +63,12 @@ public class MessageFormat2 {
 				String name = ph.getName();
 				String type = ph.getType();
 				Object value = args.get(name); // the runtime value of the placeholder
+				if (type == null || type.isEmpty()) {
+					if (value instanceof Date)
+						type = "date";
+					else if (value instanceof Number)
+						type = "number";
+				}
 
 				IFormatterFactory ffactory = DEFAULT_KNOWN_FORMATTERS.get(type);
 				if (ffactory != null) {
@@ -53,4 +81,108 @@ public class MessageFormat2 {
 		}
 		return result.toString();
 	}
+
+	private static class CaseBoth {
+		final String str;
+		final Double nr;
+		CaseBoth(String str, Double nr) {
+			this.str = str;
+			this.nr = nr;
+		}
+		@Override
+		public String toString() {
+			return "[\"" + str + "\", =" + nr + "]";
+		}
+	}
+
+	public String formatSelectorMessage(Map<String, Object> args) {
+		SelectorMessage m = mfdm.getSelectorMessage();
+//		List<String> currentCases = new ArrayList<>();
+		List<CaseBoth> ccase = new ArrayList<>();
+		for (Switch sw : m.getSwitchList()) {
+			String name = sw.getName();
+			String type = sw.getType();
+			Object nameValue = args.get(name);
+//			System.out.println(" switch: " + name + ":" + type + " => " + nameValue);
+			switch (type) {
+				case "plural":
+				case "ordinal":
+					PluralRules pr = PluralRules.forLocale(locale,
+							"ordinal".equals(type) ? PluralType.ORDINAL : PluralType.CARDINAL);
+					String sel;
+					if (nameValue instanceof Number) {
+						double dblValue = ((Number) nameValue).doubleValue();
+						sel = pr.select(dblValue);
+						ccase.add(new CaseBoth(sel, dblValue));
+					} else {
+						sel = "other";
+						ccase.add(new CaseBoth(sel, null));
+					}
+//					currentCases.add(sel);
+					break;
+				case "gender": // fall-through
+					// we should in fact check for the known values
+				case "select": // fall-through
+				default: // treat all unknown selectors as "select"
+					ccase.add(new CaseBoth(nameValue.toString(), null));
+			}
+		}
+
+//		System.out.println(ccase);
+		SimpleMessage simpleM = null;
+		SimpleMessage defaultSimpleM = null;
+		if (m.getCasesCount() != m.getMessagesCount()) { // error
+			throw new RuntimeException("TBD how we recover: selector.count != message.count");
+		}
+		for (int i = 0; i < m.getCasesCount(); i++) {
+			Cases currentCase = m.getCases(i);
+//			System.out.println("Compare: " + ccase + " to " + toString(currentCase));
+			if (currentCase.getCaseCount() != ccase.size()) { // error
+				throw new RuntimeException("TBD how we recover: selector.count != switch.count");
+			}
+			int score = 0;
+			for (int j = 0; j < ccase.size(); j++) {
+				Case c1 = currentCase.getCase(j);
+				CaseBoth c2 = ccase.get(j);
+				if (c2.nr != null && c1.getNumeric() == c2.nr) {
+					score++;
+				} else if (c1.getAlpha().equals(c2.str)) {
+					score++;
+				}
+			}
+			if (score == currentCase.getCaseCount()) {
+				simpleM = m.getMessages(i);
+			} else {
+				score = 0;
+				for (int j = 0; j < ccase.size(); j++) {
+					Case c1 = currentCase.getCase(j);
+					if ("other".equals(c1.getAlpha())) {
+						score++;
+					}
+				}
+				if (score == currentCase.getCaseCount()) {
+					defaultSimpleM = m.getMessages(i);
+				}
+			}
+		}
+//		System.out.println(simpleM);
+		if (simpleM == null)
+			simpleM = defaultSimpleM;
+		if (simpleM != null)
+			return formatSimpleMessageImpl(simpleM, args);
+		throw new RuntimeException("TBD how we recover: simple message not found");
+	}
+
+	static String toString(CasesOrBuilder value) {
+		StringJoiner joiner = new StringJoiner(", ", "[", "]");
+		joiner.setEmptyValue("[]");
+		for (Case c : value.getCaseList()) {
+			if (c.getOneofCaseCase() == Case.OneofCaseCase.ALPHA)
+				joiner.add("\"" + c.getAlpha() + "\"");
+			else
+				joiner.add("=" + c.getNumeric());
+		}
+		return joiner.toString();
+	}
+
 }
