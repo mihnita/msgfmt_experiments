7155aeac3651bc13567de515ea1f71261bc1d894
Mihai Nita <nmihai_2000@yahoo.com>
2021-01-12 10:25:58 -0800

    Using const instead of var

--- ts/test/messageformat.test.ts
+++ ts/test/messageformat.test.ts
@@ -9,27 +9,27 @@ describe('Test for MessageFormat:', () => {
 	// The tests
     it('Simple placeholder test', () => {
 		// TODO: some helper functions to make things less verbose
-		var parts = [
+		const parts = [
 			new PlainText('Hello '),
 			new Placeholder('user', '', new Map<string, string>()),
 			new PlainText('!\n')
 		];
 
-		var msgArgs = new Map<string, Object>([
+		const msgArgs = new Map<string, Object>([
 			['user', 'John']
 		]);
 
-		var expectedMsg = 'Hello John!\n';
+		const expectedMsg = 'Hello John!\n';
 		// TODO: locale should be passed to the constructor, not to format(...)
-		var mf = new SimpleMessage(parts);
+		const mf = new SimpleMessage(parts);
 		// Also a friendliner method, something that takes a JS `Object`, not a Map
-		var actual = mf.format(locale, msgArgs);
+		const actual = mf.format(locale, msgArgs);
 
         expect(expectedMsg).to.equal(actual);
     });
 
     it('Date formatting test', () => {
-		var parts = [
+		const parts = [
 			new PlainText('Using locale '),
 			new Placeholder('locale', '', new Map<string, string>()),
 			new PlainText(' the date is '),
@@ -41,20 +41,20 @@ describe('Test for MessageFormat:', () => {
 			new PlainText('.\n')
 		];
 
-		var msgArgs = new Map<string, Object>([
+		const msgArgs = new Map<string, Object>([
 			['locale', locale],
 			['theDay', new Date(2019, 11, 29)]
 		]);
 
-		var expectedMsg = 'Using locale en-IN the date is 29 Dec 2019.\n';
-		var mf = new SimpleMessage(parts);
-		var actual = mf.format(locale, msgArgs);
+		const expectedMsg = 'Using locale en-IN the date is 29 Dec 2019.\n';
+		const mf = new SimpleMessage(parts);
+		const actual = mf.format(locale, msgArgs);
 
         expect(expectedMsg).to.equal(actual);
     });
 
     it('Currency formatting test', () => {
-		var parts = [
+		const parts = [
 			new PlainText('A large currency amount is '),
 			new Placeholder('bigCount', 'number', new Map<string, string>([
 				['style', 'currency'],
@@ -63,31 +63,31 @@ describe('Test for MessageFormat:', () => {
 			new PlainText('\n')
 		];
 
-		var msgArgs = new Map<string, Object>([
+		const msgArgs = new Map<string, Object>([
 			['bigCount', 1234567890.97531]
 		]);
 
-		var expectedMsg = 'A large currency amount is €1,23,45,67,890.98\n';
-		var mf = new SimpleMessage(parts);
-		var actual = mf.format(locale, msgArgs);
+		const expectedMsg = 'A large currency amount is €1,23,45,67,890.98\n';
+		const mf = new SimpleMessage(parts);
+		const actual = mf.format(locale, msgArgs);
 
         expect(expectedMsg).to.equal(actual);
     });
 
     it('Percentage formatting test', () => {
-		var parts = [
+		const parts = [
 			new PlainText('A percentage is '),
 			new Placeholder('count', 'number', new Map<string, string>([['style', 'percent']])),
 			new PlainText('.\n')
 		];
 
-		var msgArgs = new Map<string, Object>([
+		const msgArgs = new Map<string, Object>([
 			['count', 14.2]
 		]);
 
-		var expectedMsg = 'A percentage is 1,420%.\n';
-		var mf = new SimpleMessage(parts);
-		var actual = mf.format(locale, msgArgs);
+		const expectedMsg = 'A percentage is 1,420%.\n';
+		const mf = new SimpleMessage(parts);
+		const actual = mf.format(locale, msgArgs);
 
         expect(expectedMsg).to.equal(actual);
     });
